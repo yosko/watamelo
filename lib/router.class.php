4c6d9780183e53e6b55cc7c25ee3bc0bf5b1b4df
Yosko <yosko@yosko.net>
2013-10-17 09:54:25 +0200

    modified router to handle routes from an XML instead of a JSON (may be more verbose, but definitly shorter and slightly more readable)

--- lib/router.class.php
+++ lib/router.class.php
@@ -14,9 +14,15 @@ class Router extends ApplicationComponent {
         $this->useApacheURLRewriting = $useApacheURLRewriting;
         
         //load configuration file
-        $this->file = ROOT.'/data/config/routes.json';
+        // $this->file = ROOT.'/data/config/routes.json';
+        // if (file_exists( $this->file )) {
+        //     $this->routes = json_decode(file_get_contents($this->file), true);
+        // }
+        $this->file = ROOT.'/data/config/routes.xml';
         if (file_exists( $this->file )) {
-            $this->routes = json_decode(file_get_contents($this->file), true);
+            $root = new DOMDocument('1.0', 'utf-8');
+            $root->load( $this->file );
+            $this->routes = $root->childNodes->item(0)->childNodes;
         }
     }
     
@@ -38,70 +44,74 @@ class Router extends ApplicationComponent {
         
         $foundRoute = false;
         $remainingUrl = "";
-        
+
         //check for a predifined route
-        foreach ($this->routes as $routeUrl => $route) {
-            $required = array();
-            $regexp = preg_replace_callback(
-                "/:(\w+)\|(\w+):/",
-                function ($matches) use (&$required) {
+        foreach ($this->routes as $route) {
+            if ($route->nodeType != XML_TEXT_NODE) {
+                $required = array();
+                $regexp = preg_replace_callback(
+                    "/:(\w+)\|(\w+):/",
+                    function ($matches) use (&$required) {
 
-                    $required[] = $matches[2];
+                        $required[] = $matches[2];
 
-                    //handle parameter type
-                    if($matches[1] == 'int') {
-                        return '(\d+)';
-                    } elseif($matches[1] == 'string') {
-                        return '(.+)';
-                    } else {
-                        return $matches[0];
-                    }
-                },
-                $routeUrl
-            );
-
-            //match route including required parameters
-            if( preg_match("%^".$regexp."(.*)$%i", $url, $matches) ) {
-                $foundRoute = true;
-                $parameters = array();
-                $controller = $route['controller'];
-                $action = $route['action'];
-
-                //build parameters array. The subarray 'optional' contains optional parameters
-                unset($matches[0]);
-                $optional = array_pop($matches);
-                if(!empty($required)) {
-                    $parameters = array_combine($required, $matches);
-                }
-
-                //handle additional parameters (constants given via the route definition)
-                if(isset($route['additionalParameters'])) {
-                    $parameters = array_merge($parameters, $route['additionalParameters']);
-                }
-
-                //handle optional parameters
-                if(!empty($optional)) {
-                    //if there's a "/" between required & optional part
-                    if($optional{0} == "/") {
-                        $optional = preg_split('%\|%', trim($optional, '/?'));
-                        if(count($optional) == 1 && empty($optional[0])) {
-                            unset($optional[0]);
+                        //handle parameter type
+                        if($matches[1] == 'int') {
+                            return '(\d+)';
+                        } elseif($matches[1] == 'string') {
+                            return '(.+)';
+                        } else {
+                            return $matches[0];
                         }
-                        $nbOptParam = isset($route['optionalParameters'])?count($route['optionalParameters']):0;
-                        
-                        if(count($optional) <= $nbOptParam) {
-                            //match the remaining ones to optional parameters
-                            for($i = 0; $i < $nbOptParam; $i++) {
-                                if(isset($optional[$i])) {
-                                    $parameters[$route['optionalParameters'][$i]] = $optional[$i];
+                    },
+                    $route->getAttribute('path')
+                );
+
+                //match route including required parameters
+                if( preg_match("%^".$regexp."(.*)$%i", $url, $matches) ) {
+                    $foundRoute = true;
+                    $parameters = array();
+                    $controller = $route->getAttribute('controller');
+                    $action = $route->getAttribute('action');
+
+                    //build parameters array. The subarray 'optional' contains optional parameters
+                    unset($matches[0]);
+                    $optional = array_pop($matches);
+                    if(!empty($required)) {
+                        $parameters = array_combine($required, $matches);
+                    }
+
+                    //handle additional parameters (constants given via the route definition)
+                    $additionalParameters = $route->getElementsByTagName('additional');
+                    foreach($additionalParameters as $param) {
+                        $parameters[$param->getAttribute('name')] = $param->getAttribute('value');
+                    }
+
+                    //handle optional parameters
+                    $optionalParameters = $route->getElementsByTagName('optional');
+                    if(!empty($optional)) {
+                        //if there's a "/" between required & optional part
+                        if($optional{0} == "/") {
+                            $optional = preg_split('%\|%', trim($optional, '/?'));
+                            if(count($optional) == 1 && empty($optional[0])) {
+                                unset($optional[0]);
+                            }
+                            $nbOptParam = $optionalParameters->lenght;
+                            
+                            if(count($optional) <= $nbOptParam) {
+                                //match the remaining ones to optional parameters
+                                for($i = 0; $i < $nbOptParam; $i++) {
+                                    if(isset($optional[$i])) {
+                                        $parameters[$optionalParameters->item($i)] = $optional[$i];
+                                    }
                                 }
+                            } else {
+                                //route don't really match (maybe has a longer path than the)
+                                $foundRoute = false;
                             }
                         } else {
-                            //route don't really match (maybe has a longer path than the)
                             $foundRoute = false;
                         }
-                    } else {
-                        $foundRoute = false;
                     }
                 }
             }
