2ac19081cc6dc72cd124309e93d3b0fda24310e1
Yosko <yosko@yosko.net>
2013-10-28 15:32:46 +0100

    FIX wrong call to app version in footer

--- tpl/default/footer.tpl.php
+++ tpl/default/footer.tpl.php
@@ -2,7 +2,7 @@
     </section>
 </div>
 <footer id="siteFooter">
-    <a href="https://github.com/yosko/watamelo">Watamelo, small PHP MVC framework v<?php echo $p['appVersion']; ?></a>, by <a href="http://www.yosko.net/">Yosko</a>
+    <a href="https://github.com/yosko/watamelo">Watamelo, small PHP MVC framework v<?php echo VERSION; ?></a>, by <a href="http://www.yosko.net/">Yosko</a>
 </footer>
 </body>
 </html>
