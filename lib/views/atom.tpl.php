0dea306d7c0fa28bffd50b4a9718af0e258862a2
Yosko <yosko@yosko.net>
2014-03-11 11:38:23 +0100

    minor tweaks in feed views

--- lib/views/atom.tpl.php
+++ lib/views/atom.tpl.php
@@ -4,22 +4,30 @@
     <subtitle><?php echo $feed['subtitle']; ?></subtitle>
     <link href="<?php echo $feed['self']; ?>" rel="self" />
     <link href="<?php echo $feed['link']; ?>" />
-    <id><?php echo $feed['self']; ?></id><?php if(isset($feed['self'])) { ?>
+    <id><?php echo $feed['self']; ?></id><?php
+    if(isset($feed['self'])) { ?>
 
-    <updated><?php echo $feed['self']; ?></updated><?php } foreach($feed['items'] as $key => $value) { ?>
+    <updated><?php echo $feed['updated']; ?></updated><?php
+    } //if
+    foreach($feed['items'] as $key => $value) { ?>
 
     <entry>
         <title><?php echo $value['title']; ?></title>
         <link href="<?php echo $value['link']; ?>" />
         <id><?php echo $value['link']; ?></id>
         <updated><?php echo $value['update']; ?></updated>
-        <summary type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml"><?php echo $value['summary']; ?></div></summary><?php if(isset($value['author'])) { ?>
+        <summary type="xhtml">
+            <div xmlns="http://www.w3.org/1999/xhtml"><?php echo $value['summary']; ?></div>
+        </summary><?php
+        if(isset($value['author'])) { ?>
 
         <author>
             <name><?php echo $value['author']['name']; ?></name>
             <email><?php echo $value['author']['email']; ?></email>
-        </author><?php } //if ?>
+        </author><?php
+        } //if ?>
 
-    </entry><?php } //foreach ?>
+    </entry><?php
+    } //foreach ?>
 
 </feed>
--- lib/views/rss.tpl.php
+++ lib/views/rss.tpl.php
@@ -1,22 +1,31 @@
 <?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0"
+    xmlns:content="http://purl.org/rss/1.0/modules/content/"
+    xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
         <title><?php echo $feed['title']; ?></title>
         <link><?php echo $feed['link']; ?></link>
         <description><?php echo $feed['subtitle']; ?></description>
         <language><?php echo $feed['language']; ?></language>
         <copyright><?php echo $feed['copyright']; ?></copyright>
-        <atom:link href="<?php echo $feed['self']; ?>" rel="self" type="application/rss+xml" /><?php foreach($feed['items'] as $key => $value) { ?>
+        <atom:link href="<?php echo $feed['self']; ?>" rel="self" type="application/rss+xml" /><?php
+        foreach($feed['items'] as $key => $value) { ?>
 
         <item>
             <title><?php echo $value['title']; ?></title>
-            <guid isPermaLink="<?php echo isset($value['link'])?'false':'true'; ?>"><?php echo $value['guid']; ?></guid><?php if(isset($value['link'])) { ?>
+            <guid isPermaLink="<?php echo (isset($value['link']))?'false':'true'; ?>">
+                <?php echo $value['guid']; ?>
+            </guid><?php
+            if(isset($value['link'])) { ?>
 
-            <link><?php echo $value['link']; ?></link><?php } //if ?>
+            <link><?php echo $value['link']; ?></link><?php
+            } //if ?>
 
             <pubDate><?php echo $value['pubDate']; ?></pubDate>
-            <description><![CDATA[<?php echo $value['summary']; ?>]]></description>
-        </item><?php } //foreach ?>
+            <description><![CDATA[ <?php echo $value['summary']; ?> ]]></description>
+
+        </item><?php
+        } //foreach ?>
 
     </channel>
 </rss>
